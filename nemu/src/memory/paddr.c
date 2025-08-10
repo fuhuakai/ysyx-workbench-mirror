dd6dd1660d3cbd95c02bdb2fdcb7bb180f46dda8
tracer-ysyx <tracer@ysyx.org>
2025-08-10 21:54:51 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:51 up 1 day,  2:53,  1 user,  load average: 1.10, 1.27, 0.87

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -24,6 +24,12 @@ static uint8_t *pmem = NULL;
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
+// 新增：mtrace 条件状态
+#ifdef CONFIG_MTRACE
+static bool mtrace_always = true;
+static char mtrace_cond_str[256] = "true";
+#endif
+
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
@@ -48,17 +54,73 @@ void init_mem() {
 #endif
   IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+
+    // 新增：初始化 mtrace 条件
+  #ifdef CONFIG_MTRACE
+  if (strcmp(CONFIG_MTRACE_COND, "true") != 0) {
+    mtrace_always = false;
+    strncpy(mtrace_cond_str, CONFIG_MTRACE_COND, sizeof(mtrace_cond_str)-1);
+    mtrace_cond_str[sizeof(mtrace_cond_str)-1] = '\0';
+  }
+  #endif
+}
+
+// 新增：简单的条件检查（不依赖 expr_compile）
+static inline bool mtrace_cond_met() {
+  #ifdef CONFIG_MTRACE
+  if (mtrace_always) return true;
+  
+  // 这里添加简单的条件检查逻辑
+  // 示例：只支持 "pc == 0x80000000" 这种简单条件
+  if (strstr(mtrace_cond_str, "pc == ") != NULL){
+    unsigned long target_pc;
+    if (sscanf(mtrace_cond_str, "pc == %lx", &target_pc) == 1) {
+      return cpu.pc == target_pc;
+    }
+  }
+  
+  // 默认条件不满足
+  return false;
+  #else
+  return false;
+  #endif
+}
+
+// 新增：mtrace 输出函数
+static inline void mtrace_log(char type, paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+  if (mtrace_cond_met()) {
+    printf("%c " FMT_PADDR " %d 0x%08x " FMT_WORD "\n", 
+           type, addr, len, data, cpu.pc);
+  }
+  #endif
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  if (likely(in_pmem(addr))) {
+    word_t ret = pmem_read(addr, len);
+    mtrace_log('R', addr, len, ret);
+    return ret;
+  }
+  IFDEF(CONFIG_DEVICE, {
+    word_t ret = mmio_read(addr, len);
+    mtrace_log('R', addr, len, ret);
+    return ret;
+  });
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  if (likely(in_pmem(addr))) {
+    pmem_write(addr, len, data);
+    mtrace_log('W', addr, len, data);
+    return;
+  }
+  IFDEF(CONFIG_DEVICE, {
+    mmio_write(addr, len, data);
+    mtrace_log('W', addr, len, data);
+    return;
+  });
   out_of_bound(addr);
 }
