d50d7fc8aba6d1a980e0cb107bf58a20ab8d0b1b
tracer-ysyx <tracer@ysyx.org>
2025-08-12 23:30:08 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-27-generic #27~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 22 17:38:49 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:30:08 up 1 day, 23:10,  1 user,  load average: 0.89, 0.95, 0.82

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -83,7 +83,6 @@ static int parse_args(int argc, char *argv[]) {
   };
   int o;
   
-
   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
@@ -91,7 +90,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 'e': elf_file = optarg; break; // 保存elf文件名
-      case 1: img_file = optarg; break;
+      case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
