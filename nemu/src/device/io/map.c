9f1722b3e77ba65e2fa20a0d4e3e69147615564c
tracer-ysyx <tracer@ysyx.org>
2025-08-25 16:57:45 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 25 10:47:01 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:57:45 up  5:17,  1 user,  load average: 1.62, 1.46, 1.03

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -60,7 +60,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   word_t ret = host_read(map->space + offset, len);
 
   #ifdef CONFIG_DTRACE
-    log_write("DTRACE: device read from %s at addr " FMT_PADDR ", len %d, data " FMT_WORD "\n", map->name, addr, len, ret);
+    printf("DTRACE: device read from %s at addr " FMT_PADDR ", len %d, data " FMT_WORD "\n", map->name, addr, len, ret);
   #endif
 
   return ret;
