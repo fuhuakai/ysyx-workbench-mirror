b277eecfe800c5a0c94f2f18430adac39a3f0768
tracer-ysyx <tracer@ysyx.org>
2025-08-25 19:13:31 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 25 10:47:01 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:13:31 up  7:33,  1 user,  load average: 2.18, 2.63, 1.34

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t key_code = inl(KBD_ADDR);
-  kbd->keydown = (key_code & KEYDOWN_MASK ? true :false);
+  kbd->keydown = (key_code & KEYDOWN_MASK ? true :false);//按位与结果非零证明有按键按下
   kbd->keycode = key_code & ~KEYDOWN_MASK;
 }
