9f0b023ff14c807b66579d62eaa3aa90c6d388e3
tracer-ysyx <tracer@ysyx.org>
2025-08-26 15:57:18 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 25 10:47:01 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:57:18 up 14:22,  1 user,  load average: 1.96, 1.60, 0.87

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,13 +4,23 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = 0;
+  int h = 0;
+  uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
+//读取屏幕宽度和高度
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t width_height = inl(VGACTL_ADDR);
+  uint32_t height = width_height & 0xffff;
+  uint32_t width = width_height >> 16;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = width, .height = height,
+    .vmemsz = width * height * sizeof(uint32_t)
   };
 }
 
