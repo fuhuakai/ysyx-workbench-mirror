dd25a9057842ae8c7bbbbe1c28771413a2b47d99
tracer-ysyx <tracer@ysyx.org>
2025-08-26 15:58:57 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 25 10:47:01 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:57 up 14:24,  1 user,  load average: 1.09, 1.54, 0.93

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,6 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = 0;
-  int h = 0;
-  uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
 }
 
 //读取屏幕宽度和高度
@@ -25,6 +19,29 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t size = inl(VGACTL_ADDR);
+  int vga_w = size >> 16;
+  int vga_h = size & 0xffff;
+
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w, h = ctl->h;
+  uint32_t* pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
+
+  for (int i = 0; i < h; i++) {
+    int row = y + i;
+    if (row >= vga_h) {
+      break;
+    }
+    for (int j = 0; j < w; j++) {
+      int col = x + j;
+      if (col >= vga_w) {
+        break;
+      }
+      fb[row * vga_w + col] = pixels[i*w + j];
+    }
+  }
+  
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
