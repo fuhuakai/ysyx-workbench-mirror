dae3009da216700155706bb73a25d9f1948aa98e
tracer-ysyx <tracer@ysyx.org>
2025-08-30 17:36:40 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 25 10:47:01 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:36:40 up 3 days,  6:45,  1 user,  load average: 1.54, 0.90, 0.55

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,17 +18,37 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) 
+{
+  if(direction == DIFFTEST_TO_REF)
+    memcpy(guest_to_host(RESET_VECTOR), buf, n);
+  else
+    assert(0);
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if(direction == DIFFTEST_TO_REF)
+  {
+    memcpy(&cpu, dut, sizeof(cpu));
+    // cpu.pc = dut->pc;
+    // for(int i = 0; i < 32; i++)
+    //   cpu.gpr[i] = dut->gpr[i];
+  }
+  else if(direction == DIFFTEST_TO_DUT)
+  {
+    memcpy(dut, &cpu, sizeof(cpu));
+    // dut->pc = cpu.pc;
+    // for(int i = 0; i < 32; i++)
+    //   dut->gpr[i] = cpu.gpr[i];
+  }
+  else
+    assert(0);
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
+
 
 __EXPORT void difftest_raise_intr(word_t NO) {
   assert(0);
