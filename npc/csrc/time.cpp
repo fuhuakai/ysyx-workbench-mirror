c7e66288e168193974abb89c444b433a45394ee9
tracer-ysyx <tracer@ysyx.org>
2025-09-04 13:54:38 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 25 10:47:01 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:38 up 5 days, 17:43,  1 user,  load average: 0.58, 0.26, 0.10

--- npc/csrc/time.cpp
+++ npc/csrc/time.cpp
@@ -12,7 +12,7 @@ static uint64_t get_time_internal()
   uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
   return us;
 }
-
+ 
 uint64_t get_time() 
 {
   if (boot_time == 0) boot_time = get_time_internal();
