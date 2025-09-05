4148c2ea2952791ccf6f833b26184d7b2680356e
tracer-ysyx <tracer@ysyx.org>
2025-09-05 16:14:15 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul 25 10:47:01 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:15 up 6 days, 10:53,  1 user,  load average: 0.30, 0.16, 0.13

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -46,10 +46,7 @@ extern void ebreak(int station, int inst, char unit)
 {
   if(Verilated::gotFinish())
     return;
-    // Log("maintime = %ld, state = %d, pc = 0x%08x, inst = 0x%08x", main_time, npc_state.state, top->rootp->rv32__DOT__pc, top->rootp->rv32__DOT__inst);
-
-  //虽然波形图上inst随pc同时变化，但通过打印二者会发现inst会在pc变化之后才改变（这是因为二者都发生变化了之后才输出至波形图的）
-  //然而，这个延时会导致decode错误，然后调用了 “ebreak(`ABORT, inst);”
+    
   if(main_time >= start_time + 1)   // at the begining (main_time < start_time and before the reset), all regs are zeros
   {
     npc_state.halt_ret = top->rootp->rv32__DOT__register_file_inst__DOT__regs[10]; //a0
