76cbb746fa8978584dfb4997a2e0bc1f429075f8
tracer-ysyx <tracer@ysyx.org>
2025-09-06 15:05:54 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:05:54 up  1:16,  1 user,  load average: 0.53, 0.41, 0.25

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -113,13 +113,13 @@ void pmem_write(int waddr, int wdata, char wmask)
     return;
 
   // device serial
-  // if(waddr == CONFIG_SERIAL_MMIO)
-  // {
-  //   assert(wmask == WByte);
-  //   char ch = (char)wdata;
-  //   putchar(ch);
-  //   return;
-  // }
+  if(waddr == CONFIG_SERIAL_MMIO)
+  {
+    assert(wmask == WByte);
+    char ch = (char)wdata;
+    putchar(ch);
+    return;
+  }
 
   switch (wmask)
   {
