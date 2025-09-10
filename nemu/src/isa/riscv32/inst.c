0d66ddcaf46a784eed1a936ddc4426bb4daa83f5
tracer-ysyx <tracer@ysyx.org>
2025-09-10 14:36:01 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:36:01 up 1 day, 21:17,  1 user,  load average: 0.07, 0.21, 0.39

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -156,8 +156,8 @@ static int decode_exec(Decode *s) {
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
-  //INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s -> dnpc = isa_raise_intr(11, s -> pc));//自陷指令
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ECALL(s->dnpc));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s -> dnpc = isa_raise_intr(11, s -> pc));//自陷指令
+  //INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ECALL(s->dnpc));
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = CSR(0x341));
   INSTPAT_END();
 
