622bdfd6f9e886cf528b2bd63bed5ff83ba451c7
tracer-ysyx <tracer@ysyx.org>
2025-09-11 15:45:25 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:45:25 up 2 days, 13:40,  1 user,  load average: 1.26, 0.74, 0.47

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -32,10 +32,23 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *cp = (Context *)(kstack.end - sizeof(Context));
-  //memset(cp, 0, sizeof(Context));
-  cp->mepc = (uintptr_t)entry - 4;
-  return cp;
+  // Context *cp = (Context *)(kstack.end - sizeof(Context));
+  // //memset(cp, 0, sizeof(Context));
+  // cp->mepc = (uintptr_t)entry - 4;
+  // return cp;
+    //栈底在上，栈顶在下
+  Context *c = kstack.end - 4 - sizeof(Context);
+  
+  c->mcause = 0xb;
+  c->mstatus = 0x1800;
+  //入口为f()
+  c->mepc = (uint32_t)entry;
+  for(int i = 0; i < NR_REGS; i++)
+    c->gpr[i] = 0;
+  
+  //观察汇编，a0为传参寄存器
+  c->gpr[10] = (uint32_t)arg;
+  return c;
 }
 
 void yield() {
