19d5593e53beba6c1865123716c27bd9288817cb
tracer-ysyx <tracer@ysyx.org>
2025-09-17 15:24:25 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:25 up  6:50,  1 user,  load average: 1.71, 1.15, 0.65

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -137,14 +137,18 @@ void cpu_exec(uint64_t n)
 
     execute(n);
 
+      // 出错时打印环形缓冲区
+    #ifdef CONFIG_IRINGBUF
+    if (npc_state.state == NPC_ABORT) {
+        display_iringbuf();
+     }
+    #endif
+
     switch (npc_state.state) 
     {
         case NPC_RUNNING: npc_state.state = NPC_STOP; break;
 
         case NPC_END: case NPC_ABORT:
-#ifdef CONFIG_IRINGBUF 
-            display_iringbuf();
-#endif
             Log("NPC: %s at pc = 0x%08x",
                 (npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
                 (npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
