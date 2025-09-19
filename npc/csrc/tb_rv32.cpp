91f393730d8371b767fd2085d25c34c96696ed36
tracer-ysyx <tracer@ysyx.org>
2025-09-19 22:35:47 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:35:47 up 1 day, 18:47,  1 user,  load average: 1.19, 0.88, 1.06

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -88,9 +88,9 @@ extern void ebreak(int station, int inst, char unit)
 #ifdef CONFIG_ETRACE
 extern void etrace(int inst)
 {
-  // _Log(ANSI_FG_YELLOW "[etrace]" ANSI_NONE ANSI_FG_YELLOW "mstatus:" ANSI_NONE "0x%08x, "
-  //      ANSI_FG_YELLOW "mepc:"    ANSI_NONE " 0x%08x, " ANSI_FG_YELLOW "mcause:" ANSI_NONE " 0x%08x\n", 
-  //     top_mstatus, top_mepc, top_mcause);
+  _Log(ANSI_FG_YELLOW "[etrace]" ANSI_NONE ANSI_FG_YELLOW "mstatus:" ANSI_NONE "0x%08x, "
+       ANSI_FG_YELLOW "mepc:"    ANSI_NONE " 0x%08x, " ANSI_FG_YELLOW "mcause:" ANSI_NONE " 0x%08x\n", 
+      top_mstatus, top_mepc, top_mcause);
 }
 #endif  
 
