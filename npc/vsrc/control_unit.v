c473f489465e51aff992399dccef7f06b03ae8a0
tracer-ysyx <tracer@ysyx.org>
2025-09-19 22:31:41 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:31:41 up 1 day, 18:43,  1 user,  load average: 0.61, 1.17, 1.20

--- npc/vsrc/control_unit.v
+++ npc/vsrc/control_unit.v
@@ -36,8 +36,7 @@ module control_unit(
     always @(*) begin
         // Set default values for all output signals
 
-    is_ecall = `FALSE;
-    csr_wen = `WDisen;
+  
 
         case(opcode_6_0)
             `INST_TYPE_R: begin
