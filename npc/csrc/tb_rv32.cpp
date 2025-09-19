645252e3a777ec5961eae9414f8e15c549191271
tracer-ysyx <tracer@ysyx.org>
2025-09-19 17:20:15 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:20:15 up 1 day, 13:31,  1 user,  load average: 1.22, 0.51, 0.41

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -86,9 +86,11 @@ extern void ebreak(int station, int inst, char unit)
 #define top_mcause    top->rootp->rv32__DOT__csr_regs_inst__DOT__mcause
 extern void etrace(int inst)
 {
+  #ifdef CONFIG_ETRACE
   _Log(ANSI_FG_YELLOW "[etrace]" ANSI_NONE ANSI_FG_YELLOW "mstatus:" ANSI_NONE "0x%08x, "
        ANSI_FG_YELLOW "mepc:"    ANSI_NONE " 0x%08x, " ANSI_FG_YELLOW "mcause:" ANSI_NONE " 0x%08x\n", 
       top_mstatus, top_mepc, top_mcause);
+  #endif
 }
 
 extern int pmem_read(int raddr)
