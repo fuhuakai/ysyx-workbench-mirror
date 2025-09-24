75323bc282057a940a1584ca64c3120878af63ed
tracer-ysyx <tracer@ysyx.org>
2025-09-24 22:16:26 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:26 up 21:03,  1 user,  load average: 0.46, 0.38, 0.44

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -175,10 +175,11 @@ void single_cycle(void)
   }
 }
 
-static void reset(void) {
-  top->rst = 1;
-  for (int i = 0; i < 10; i++) single_cycle();
-  top->rst = 0;
+static void reset(void)
+{
+  top->rst = 0; single_cycle();
+  top->rst = 1; single_cycle();
+  top->rst = 0; single_cycle();
 }
 
 static void init_verilator(void)
