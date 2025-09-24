d414906b082d7e0bed5abd304f430f6617401665
tracer-ysyx <tracer@ysyx.org>
2025-09-25 00:29:49 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:29:49 up 23:17,  1 user,  load average: 1.17, 0.30, 0.15

--- npc/vsrc/pc.v
+++ npc/vsrc/pc.v
@@ -1,1 +1,42 @@
 `include "defines.v"
+module PC(
+  input  wire           clk,
+  input  wire           rst,
+  input  wire           pc_sel_1,
+  input  wire [1:0]     pc_sel_2,
+  input  wire [`RegBus] result,
+  input  wire [`RegBus] imm32,
+  input  wire [`RegBus] csr_npc,
+  output wire [`RegBus] PCadd4,
+  output reg  [`RegBus] pc
+);
+
+  wire [`RegBus] npc;
+  wire [`RegBus] npc_temp;
+  wire [`RegBus] PCaddIMM32;
+  
+  assign PCadd4     = pc + `PC_INCREMENT;
+  assign PCaddIMM32 = pc + imm32;
+
+  always @(posedge clk) begin
+    if(rst == `RST_VAL)
+      pc <= `RESET_VECTOR;
+    else 
+      pc <= npc;
+  end
+
+  // MUX1 module
+  MuxKey #(2, 1, `BitWidth) i1(npc, (pc_sel_1 & result[0]), {
+      `MUX1_NBpc, npc_temp,
+      `MUX1_Bpc, PCaddIMM32}
+  );
+
+  // MUX2 module
+  MuxKey #(4, 2, `BitWidth) i2(npc_temp, (pc_sel_2), {
+      `MUX2_PCadd4, PCadd4,
+      `MUX2_result, result,
+      `MUX2_csrnpc, csr_npc,
+      `MUX2_IDLE,   32'hdeadbeef}
+  );
+
+endmodule
