ad7899d7b6357270f05da728c9a3474a4125c4c0
tracer-ysyx <tracer@ysyx.org>
2025-09-25 23:29:56 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:29:56 up 14:00,  1 user,  load average: 1.35, 0.67, 0.44

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -12,14 +12,10 @@ extern vluint64_t main_time;
 
 uint8_t pmem[PMEM_SIZE] PG_ALIGN = {};
 static const word_t img [] = {
-
+  0x06458613,    //addi	a2,a1,100
+  0x0c860693,    //addi	a3,a2,200
   0x00000297,    // auipc t0,0
-  0x00000297,    // auipc t0,0
-  0x00000297,    // auipc t0,0
-  0x00000297,    // auipc t0,0
-  // 0x00000513,    //	li	a0,0
-   0x06458613,    //addi	a2,a1,100
-  // 0x0c860693,    //addi	a3,a2,200
+  0x00000513,    //	li	a0,0
   0x00100073,    // ebreak 
   0xdeadbeef,    // some data
 };
