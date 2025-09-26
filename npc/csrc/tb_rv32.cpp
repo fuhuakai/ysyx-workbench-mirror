beca82f6246e5c3cbb40f58ba0b3113571328980
tracer-ysyx <tracer@ysyx.org>
2025-09-26 23:11:40 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:40 up 1 day, 13:42,  1 user,  load average: 0.29, 0.29, 0.28

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -358,7 +358,7 @@ extern int dmem_read(int raddr)
   static int data = 0xdead000a;
 
   // 因为是是周期CPU，所以理论上来说应该轮到LSU工作的时候才读/写dmem
-  if(main_time < start_time || top->rootp->rv32__DOT__clk_cnt != 4)
+  if(main_time < start_time || top->rootp->rv32__DOT__clk_cnt != 3)
     return data;
 
   // device rtc
