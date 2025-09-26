f8f6da05b1b6a6848a1f0b60fa84d691e10e8596
tracer-ysyx <tracer@ysyx.org>
2025-09-26 19:39:59 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:39:59 up 1 day, 10:10,  1 user,  load average: 0.18, 0.24, 0.16

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -341,15 +341,15 @@ extern int imem_read(int raddr)
 
 
   data = pmem_r(raddr, 4);
-   // 添加指令跟踪
-   if (raddr < 0x80000000) {
-    printf("out of bound: time=%ld, pc=0x%08x, inst=0x%08x\n", 
-           main_time, raddr, data);
-  }
-  if (raddr >= 0x80000000 && raddr <= 0x87ffffff) {
-    printf("IMEM_READ: time=%ld, pc=0x%08x, inst=0x%08x\n", 
-           main_time, raddr, data);
-  }
+  //  // 添加指令跟踪
+  //  if (raddr < 0x80000000) {
+  //   printf("out of bound: time=%ld, pc=0x%08x, inst=0x%08x\n", 
+  //          main_time, raddr, data);
+  // }
+  // if (raddr >= 0x80000000 && raddr <= 0x87ffffff) {
+  //   printf("IMEM_READ: time=%ld, pc=0x%08x, inst=0x%08x\n", 
+  //          main_time, raddr, data);
+  // }
   return data;    
 }
 
