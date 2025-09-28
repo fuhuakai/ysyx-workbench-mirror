4c717bd725af62694bbc76971a21cf2a1f13b8a9
tracer-ysyx <tracer@ysyx.org>
2025-09-28 17:42:56 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:56 up 3 days,  8:13,  1 user,  load average: 0.36, 0.14, 0.17

--- npc/obj_dir/Vrv32__Dpi.h
+++ npc/obj_dir/Vrv32__Dpi.h
@@ -18,11 +18,11 @@ extern "C" {
     // DPI IMPORTS
     // DPI import at /home/fuhuakai/ysyx-workbench/npc/vsrc/idu.v:43:34
     extern void TRAP(int station, char unit);
-    // DPI import at /home/fuhuakai/ysyx-workbench/npc/vsrc/lsu.v:57:34
+    // DPI import at /home/fuhuakai/ysyx-workbench/npc/vsrc/lsu.v:316:34
     extern int dmem_read(int raddr);
     // DPI import at /home/fuhuakai/ysyx-workbench/npc/vsrc/ifu.v:19:34
     extern int imem_read(int raddr);
-    // DPI import at /home/fuhuakai/ysyx-workbench/npc/vsrc/lsu.v:58:34
+    // DPI import at /home/fuhuakai/ysyx-workbench/npc/vsrc/lsu.v:317:34
     extern void pmem_write(int waddr, int wdata, char wmask);
 
 #ifdef __cplusplus
