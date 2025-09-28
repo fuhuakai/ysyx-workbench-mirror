2f81f2fec73be00f0a2ec9906b35602caa6acfca
tracer-ysyx <tracer@ysyx.org>
2025-09-28 10:45:12 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:12 up 3 days,  1:15,  1 user,  load average: 0.71, 0.34, 0.25

--- npc/csrc/tb_rv32.cpp
+++ npc/csrc/tb_rv32.cpp
@@ -383,9 +383,9 @@ extern int dmem_read(int raddr)
 
 void pmem_write(int waddr, int wdata, char wmask)
 {
-  // // 因为是是周期CPU，所以理论上来说应该轮到LSU工作的时候才读/写dmem
-  // if(main_time < start_time || top->rootp->rv32__DOT__clk_cnt != 3)
-  //   return;
+  // 因为是是周期CPU，所以理论上来说应该轮到LSU工作的时候才读/写dmem
+  if(main_time < start_time || top->rootp->rv32__DOT__clk_cnt != 3)
+    return;
 
   // device serial
   if(waddr == CONFIG_SERIAL_MMIO)
