1dda91c1ef4def1c4a70c5548bb541fee3b646a5
tracer-ysyx <tracer@ysyx.org>
2025-09-30 00:38:04 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:38:04 up 4 days, 15:08,  1 user,  load average: 0.13, 0.06, 0.09

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -58,14 +58,18 @@ static void statistic() {
 static void execute_once() 
 {
     uint64_t ifu_pack = top->rootp->rv32__DOT__ifu_inst__DOT__ifu_valid_data_reg;
-    PCSet.pc    = (uint32_t)(ifu_pack >> 32);
+    //PCSet.pc    = (uint32_t)(ifu_pack >> 32);
     PCSet.inst  = (uint32_t)(ifu_pack & 0xffffffffu);
-    //PCSet.pc = top->rootp->rv32__DOT__bru_inst__DOT__npc_reg;  PCSet.inst = top->rootp->rv32__DOT__ifu_inst__DOT__ifu_inst;
+    
+    PCSet.pc = top->rootp->rv32__DOT__bru_inst__DOT__npc_reg;  
+    //PCSet.inst = top->rootp->rv32__DOT__ifu_inst__DOT__ifu_inst;
     single_cycle();  single_cycle();single_cycle();single_cycle();single_cycle();single_cycle();
     ifu_pack = top->rootp->rv32__DOT__ifu_inst__DOT__ifu_valid_data_reg; 
-    PCSet.pc    = (uint32_t)(ifu_pack >> 32);
+    //PCSet.npc    = (uint32_t)(ifu_pack >> 32);
     PCSet.inst  = (uint32_t)(ifu_pack & 0xffffffffu);
-    //PCSet.npc = top->rootp->rv32__DOT__bru_inst__DOT__npc_reg;  PCSet.ninst = top->rootp->rv32__DOT__ifu_inst__DOT__ifu_inst;
+
+    PCSet.npc = top->rootp->rv32__DOT__bru_inst__DOT__npc_reg; 
+    //PCSet.ninst = top->rootp->rv32__DOT__ifu_inst__DOT__ifu_inst;
 
 #ifdef CONFIG_ITRACE
     // 将指令拆分为字节
