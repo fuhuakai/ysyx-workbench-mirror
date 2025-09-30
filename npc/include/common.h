5076582194b71f9353a1641d62564b12122b7d75
tracer-ysyx <tracer@ysyx.org>
2025-09-30 16:25:42 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Aug 14 16:52:50 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:42 up 5 days,  6:56,  1 user,  load average: 0.81, 0.34, 0.34

--- npc/include/common.h
+++ npc/include/common.h
@@ -71,7 +71,7 @@
 // #define CONFIG_ETRACE   1
 //  #define CONFIG_IRINGBUF 1
  //#define CONFIG_DIFFTEST 1
- #define CONFIG_WAVES   1
+ //#define CONFIG_WAVES   1
 
 #define MAX_iringbuf_size  20
 
