bbff226e45fdcb71c11a471494ba823d76925811
tracer-ysyx <tracer@ysyx.org>
2025-10-10 10:20:27 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-33-generic #33~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 17:02:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:20:27 up 4 days, 14:36,  1 user,  load average: 0.20, 0.07, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -40,7 +40,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   c->mstatus = 0x1800;
   
   //a0为传参寄存器
-  c->gpr[11] = (uint32_t)arg;
+  c->gpr[12] = (uint32_t)arg;
   return c;
 }
 
