73455800a1c6bf25f4fb8403a67fb5585dfe0ca9
tracer-ysyx <tracer@ysyx.org>
2025-10-21 22:56:18 +0800

    > compile NEMU ysyx_22040000 付华凯 Linux fuhuakai-VMware-Virtual-Platform 6.14.0-33-generic #33~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 17:02:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:56:18 up 11:04,  1 user,  load average: 0.34, 0.15, 0.10

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,18 +1,11 @@
-// #include <am.h>
-// #include "../riscv.h"
-
-// #define KBD_ADDR 0xa0000060
-// #define KEYDOWN_MASK 0x8000
-
-// void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-//   uint32_t key_code = inl(KBD_ADDR);
-//   kbd->keydown = (key_code & KEYDOWN_MASK ? true :false);//按位与结果非零证明有按键按下
-//   kbd->keycode = key_code & ~KEYDOWN_MASK;
-// }
-
 #include <am.h>
+#include "../riscv.h"
+
+#define KBD_ADDR 0xa0000060
+#define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key_code = inl(KBD_ADDR);
+  kbd->keydown = (key_code & KEYDOWN_MASK ? true :false);//按位与结果非零证明有按键按下
+  kbd->keycode = key_code & ~KEYDOWN_MASK;
 }
